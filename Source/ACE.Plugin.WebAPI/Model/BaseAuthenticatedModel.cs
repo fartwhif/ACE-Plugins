d9f268e9efb2dffa05c88815d4f2d121d5ca32f5
fartwhif <fartwhif@tuta.io>
2022-08-02 20:02:43 -0500

    Remove depreciated plugin 'WebAPI'

--- Source/ACE.Plugin.WebAPI/Model/BaseAuthenticatedModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ACE.Plugin.WebAPI.Model
-{
-    public class BaseAuthenticatedModel
-    {
-        public string AccountName { get; set; }
-        public string AccessLevelName { get; set; }
-    }
-}
--- Source/ACE.Plugin.WebAPI/Model/PlayerCount.cs
+++
@@ -1,8 +0,0 @@
-namespace ACE.Plugin.WebAPI.Model
-{
-    public class PlayerCountResponseModel
-    {
-        public int Online { get; set; }
-        public int Offline { get; set; }
-    }
-}
