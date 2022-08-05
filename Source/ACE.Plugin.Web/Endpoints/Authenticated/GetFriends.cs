2c4a153dd541e459d6b25aa23f87a6aacc478d44
fartwhif <fartwhif@tuta.io>
2022-08-04 19:27:06 -0500

    Completed GetFriends endpoint

---
+++ Source/ACE.Plugin.Web/Endpoints/Authenticated/GetFriends.cs
@@ -0,0 +1,56 @@
+﻿using ACE.Database;
+using ACE.Database.Models.Shard;
+using ACE.Plugin.Web.Model;
+using ACE.Server.Managers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace ACE.Plugin.Web
+{
+    static partial class WebEndpoints
+    {
+        public static IEndpointRouteBuilder GetFriends(IEndpointRouteBuilder endpoints)
+        {
+            _ = endpoints.MapGet("GetFriends", (Func<HttpContext, Task<string>>)(async (context) =>
+            {
+                var account = context.User.ToACEAccount();
+                FriendsList frens = new();
+                Gate.RunGatedAction(() =>
+                {
+                    TaskCompletionSource<object> tsc = new TaskCompletionSource<object>();
+                    List<Character>? characters = null;
+                    DatabaseManager.Shard.GetCharacters(account.AccountId, true, (chars) =>
+                    {
+                        characters = chars;
+                        tsc.SetResult(new object());
+                    });
+                    tsc.Task.Wait();
+
+                    List<uint> allFriends = new();
+                    ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+                    List<CharacterPropertiesFriendList> friends = new List<CharacterPropertiesFriendList>();
+                    foreach (var c in characters)
+                    {
+                        friends.AddRange(c.GetFriends(rwLock));
+                    }
+                    friends = friends.GroupBy(k => k.CharacterId).Select(k => k.First()).ToList();
+                    foreach (var f in friends)
+                    {
+                        var player = PlayerManager.FindByGuid(f.FriendId, out var isOnline);
+                        if (isOnline)
+                        {
+                            frens.Online.Add(player.Name);
+                        }
+                        else
+                        {
+                            frens.Offline.Add(player.Name);
+                        }
+                    }
+                });
+                return context.Ok(frens);
+            })).RequireAuthorization();
+            return endpoints;
+        }
+    }
+}
---
+++ Source/ACE.Plugin.Web/Model/FriendsList.cs
@@ -0,0 +1,8 @@
+﻿namespace ACE.Plugin.Web.Model
+{
+    public class FriendsList
+    {
+        public List<string> Online = new List<string>();
+        public List<string> Offline = new List<string>();
+    }
+}
